953c70f364c681383e5b94c24ddd354c39f20797
Janis <ignatov.vit@gmail.com>
2014-12-02 17:19:55 +0300

    test

---
+++ data/catalogues_list.js
@@ -0,0 +1,47 @@
+{
+  "version": 1,
+  "catalogues": [
+    {
+      "title": "Commemorative 2 Euro coins of European Union",
+      "count": 251,
+      "date": "2014-11-21",
+      "lang": "en",
+      "file": "std-commemorative-eu.db"
+    },
+    {
+      "title": "Types of circulation coins of European Union",
+      "count": 328,
+      "date": "2014-11-27",
+      "lang": "en",
+      "file": "base-regular-eu.db"
+    },
+    {
+      "title": "Commemorative US coins",
+      "count": 268,
+      "date": "2014-11-10",
+      "lang": "en",
+      "file": "std-commemorative-us.db"
+    },
+    {
+      "title": "Base Commemorative US coins",
+      "count": 134,
+      "date": "2014-11-10",
+      "lang": "en",
+      "file": "base-commemorative-us.db"
+    },
+    {
+      "title": "Памятные монеты Российской Федерации",
+      "count": 212,
+      "date": "2014-11-24",
+      "lang": "ru",
+      "file": "std-commemorative-ru.db"
+    },
+    {
+      "title": "Памятные монеты СССР",
+      "count": 68,
+      "date": "2014-11-19",
+      "lang": "ru",
+      "file": "std-commemorative-su.db"
+    }
+  ]
+}
