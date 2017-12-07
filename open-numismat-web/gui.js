84ab394485b1acbe7e7cd758541ef1653200aaf5
Виталий Игнатов <ignatov.vit@gmail.com>
2017-12-07 16:28:54 +0300

    Add Online viewer

---
+++ open-numismat-web/gui.js
@@ -0,0 +1,98 @@
+var outputElm = document.getElementById('output');
+var errorElm = document.getElementById('error');
+var dbFileElm = document.getElementById('dbfile');
+
+// Start the worker in which sql.js will run
+var worker = new Worker("js/worker.sql.js");
+worker.onerror = error;
+
+// Open a database
+worker.postMessage({action:'open'});
+
+// Connect to the HTML element we 'print' to
+function print(text) {
+    outputElm.innerHTML = text.replace(/\n/g, '<br>');
+}
+function error(e) {
+  console.log(e);
+	errorElm.style.height = '2em';
+	errorElm.textContent = e.message;
+}
+
+function noerror() {
+		errorElm.style.height = '0';
+}
+
+// Run a command in the database
+function execute(commands) {
+	tic();
+	worker.onmessage = function(event) {
+		var results = event.data.results;
+		toc("Executing SQL");
+
+		tic();
+		outputElm.innerHTML = "";
+		for (var i=0; i<results.length; i++) {
+			outputElm.appendChild(tableCreate(results[i].columns, results[i].values));
+		}
+		toc("Displaying results");
+	}
+	worker.postMessage({action:'exec', sql:commands});
+	outputElm.textContent = "Fetching results...";
+}
+
+// Create an HTML table
+var tableCreate = function () {
+  function valconcat(vals, tagName) {
+    if (vals.length === 0) return '';
+    var open = '<'+tagName+'>', close='</'+tagName+'>';
+    return open + vals.join(close + open) + close;
+  }
+  function _arrayBufferToBase64( buffer ) {
+    var binary = '';
+    var bytes = new Uint8Array( buffer );
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+        binary += String.fromCharCode( bytes[ i ] );
+    }
+    return window.btoa( binary );
+  }
+  return function (columns, values){
+    var tbl  = document.createElement('table');
+    var html = '<thead>' + valconcat(columns, 'th') + '</thead>';
+    var rows = values.map(function(v){ return '<td><img src="data:image/png;base64,' + _arrayBufferToBase64(v[0]) + '"></td><td>' + v[1] + '</td><td>' + v[2] + '</td>'; });
+    html += '<tbody>' + valconcat(rows, 'tr') + '</tbody>';
+	  tbl.innerHTML = html;
+    return tbl;
+  }
+}();
+
+// Performance measurement functions
+var tictime;
+if (!window.performance || !performance.now) {window.performance = {now:Date.now}}
+function tic () {tictime = performance.now()}
+function toc(msg) {
+	var dt = performance.now()-tictime;
+	console.log((msg||'toc') + ": " + dt + "ms");
+}
+
+// Load a db from a file
+dbFileElm.onchange = function() {
+	var f = dbFileElm.files[0];
+	var r = new FileReader();
+	r.onload = function() {
+		worker.onmessage = function () {
+			toc("Loading database from file");
+            noerror()
+            execute ("SELECT images.image, title, status FROM coins INNER JOIN images on images.id = coins.image;");
+		};
+		tic();
+		try {
+			worker.postMessage({action:'open',buffer:r.result}, [r.result]);
+		}
+		catch(exception) {
+			worker.postMessage({action:'open',buffer:r.result});
+		}
+	}
+	r.readAsArrayBuffer(f);
+}
