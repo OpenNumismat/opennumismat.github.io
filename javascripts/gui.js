73cba306fabd249d6f3f1d55f60bd98d00112c08
Виталий Игнатов <ignatov.vit@gmail.com>
2017-12-14 15:44:33 +0300

    i18n

--- javascripts/gui.js
+++ javascripts/gui.js
@@ -35,6 +35,403 @@ function status(text="") {
     }
 }
 
+i18next.init({
+  lng: $('#lang').text(),
+  debug: true,
+  resources: {
+    en: {
+      translation: {
+          status: "Status",
+          country: "Country",
+          series: "Series",
+          type: "Type",
+          period: "Period",
+          All: "All",
+          owned: "Owned",
+          demo: "Demo",
+          pass: "Pass",
+          ordered: "Ordered",
+          sold: "Sold",
+          sale: "Sale",
+          wish: "Wish",
+          region: "Region",
+          ruler: "Ruler",
+          denomination: "Denomination",
+          subject: "Subject",
+          date_issue: "Date of issue",
+          year: "Year",
+          mintage: "Mintage",
+          material: "Material",
+          mint: "Mint",
+      }
+    },
+    bg: {
+      translation: {
+          "status": "Статус",
+          "country": "Държава",
+          "series": "Серия",
+          "type": "Type",
+          "period": "Период",
+          "All": "Всички",
+          "owned": "В наличност",
+          "demo": "Демонстрация",
+          "pass": "Pass",
+          "ordered": "Поръчана",
+          "sold": "Продадена",
+          "sale": "Продава се",
+          "wish": "Желана",
+          "region": "Region",
+          "ruler": "Владетел",
+          "denomination": "Denomination",
+          "subject": "Тема",
+          "date_issue": "Дата на издаване",
+          "year": "Година",
+          "mintage": "Тираж",
+          "material": "Материал",
+          "mint": "Монетен двор"
+      }
+    },
+    ca: {
+      translation: {
+          "status": "Estatus",
+          "country": "País",
+          "series": "Sèrie",
+          "type": "Tipus",
+          "period": "Període",
+          "All": "Tots",
+          "owned": "Pròpies",
+          "demo": "Demo",
+          "pass": "Subhasta",
+          "ordered": "Comprades",
+          "sold": "Venudes",
+          "sale": "Venda",
+          "wish": "Desitjos",
+          "region": "Region",
+          "ruler": "Ruler",
+          "denomination": "Denominació",
+          "subject": "Subjecte",
+          "date_issue": "Data d'emissió",
+          "year": "Any",
+          "mintage": "Encunyació",
+          "material": "Material",
+          "mint": "Seca"
+        }
+    },
+    cs: {
+      translation: {
+          "status": "Stav",
+          "country": "Země",
+          "series": "Série",
+          "type": "Typ",
+          "period": "Období",
+          "All": "All",
+          "owned": "Vlastněno",
+          "demo": "Ukázka",
+          "pass": "Projít",
+          "ordered": "Objednáno",
+          "sold": "Prodáno",
+          "sale": "Prodej",
+          "wish": "Přání",
+          "region": "Region",
+          "ruler": "Ruler",
+          "denomination": "Hodnota",
+          "subject": "Předmět",
+          "date_issue": "Datum vystavení",
+          "year": "Rok",
+          "mintage": "Mintage",
+          "material": "Materiál",
+          "mint": "Mincovna"
+        }
+    },
+    de: {
+      translation: {
+          "status": "Status",
+          "country": "Land",
+          "series": "Serie",
+          "type": "Typ",
+          "period": "Periode",
+          "All": "All",
+          "owned": "Vorhanden",
+          "demo": "Demo",
+          "pass": "Beobachtet",
+          "ordered": "Bestellt",
+          "sold": "Verkauft",
+          "sale": "Verkauf",
+          "wish": "Gesucht",
+          "region": "Region",
+          "ruler": "Ruler",
+          "denomination": "Denomination",
+          "subject": "Betreff",
+          "date_issue": "Ausgabedatum",
+          "year": "Jahr",
+          "mintage": "Auflage",
+          "material": "Material",
+          "mint": "Münzanstalt"
+        }
+    },
+    el: {
+      translation: {
+          "status": "Κατάσταση",
+          "country": "Χώρα",
+          "series": "Σειρά",
+          "type": "Τύπος",
+          "period": "Περίοδος",
+          "All": "Όλα",
+          "owned": "Ιδιόκτητο",
+          "demo": "Επίδειξης",
+          "pass": "Προσωρινό",
+          "ordered": "Παραγγέλθηκε",
+          "sold": "Πωλείται",
+          "sale": "Πώληση",
+          "wish": "Επιθυμία",
+          "region": "Περιοχή",
+          "ruler": "Χάρακας",
+          "denomination": "Ονομαστική αξία",
+          "subject": "Θέμα",
+          "date_issue": "Ημερ/νία έκδοσης",
+          "year": "Έτος",
+          "mintage": "Νομισματοκοπία",
+          "material": "Υλικό",
+          "mint": "Νομισματοκοπείο"
+        }
+    },
+    es: {
+      translation: {
+          "status": "Estatus",
+          "country": "País",
+          "series": "Series",
+          "type": "Tipo",
+          "period": "Período",
+          "All": "Todo",
+          "owned": "Propia",
+          "demo": "Demo",
+          "pass": "Pasar",
+          "ordered": "Pedida",
+          "sold": "Vendida",
+          "sale": "Vender",
+          "wish": "Deseada",
+          "region": "Región",
+          "ruler": "Regla",
+          "denomination": "Denominación",
+          "subject": "Motivo",
+          "date_issue": "Fecha de emisión",
+          "year": "Año",
+          "mintage": "Acuñación",
+          "material": "Material",
+          "mint": "Ceca"
+        }
+    },
+    fr: {
+      translation: {
+          "status": "Statut",
+          "country": "Pays",
+          "series": "Séries",
+          "type": "Type",
+          "period": "Période",
+          "All": "Tous",
+          "owned": "Acquise",
+          "demo": "Démonstration",
+          "pass": "Pass",
+          "ordered": "Commandé",
+          "sold": "Vendu",
+          "sale": "Vente",
+          "wish": "Envie",
+          "region": "Region",
+          "ruler": "Ruler",
+          "denomination": "Dénomination",
+          "subject": "Sujet",
+          "date_issue": "Date d'émission",
+          "year": "Année",
+          "mintage": "Mintage",
+          "material": "Matériau",
+          "mint": "Monnaie"
+        }
+    },
+    hu: {
+      translation: {
+          "status": "Státusz",
+          "country": "Ország",
+          "series": "Sorozat",
+          "type": "Típus",
+          "period": "Koszak",
+          "All": "All",
+          "owned": "Tulajdonos",
+          "demo": "Demó",
+          "pass": "Átmeneti",
+          "ordered": "Ordered",
+          "sold": "Eladott",
+          "sale": "Eladó",
+          "wish": "Megszerzendő",
+          "region": "Region",
+          "ruler": "Ruler",
+          "denomination": "Denomination",
+          "subject": "Tárgy",
+          "date_issue": "Kiadás éve",
+          "year": "Év",
+          "mintage": "Pénzverde",
+          "material": "Anyag",
+          "mint": "Pénzverde"
+        }
+    },
+    it: {
+      translation: {
+          "status": "Condizione",
+          "country": "Nazione",
+          "series": "Serie",
+          "type": "Tipo",
+          "period": "Periodo",
+          "All": "Tutto",
+          "owned": "Posseduta",
+          "demo": "Demo",
+          "pass": "Scambio",
+          "ordered": "Ordinata",
+          "sold": "Venduta",
+          "sale": "Vendita",
+          "wish": "Desiderata",
+          "region": "Region",
+          "ruler": "Ruler",
+          "denomination": "Denominazione",
+          "subject": "Soggetto",
+          "date_issue": "Data di emissione",
+          "year": "Anno",
+          "mintage": "Coniatura",
+          "material": "Materiale",
+          "mint": "Condizione"
+        }
+    },
+    nl: {
+      translation: {
+          "status": "Status",
+          "country": "Land",
+          "series": "Uitvoering",
+          "type": "Type",
+          "period": "Periode",
+          "All": "Alle",
+          "owned": "Eigendom",
+          "demo": "Demo",
+          "pass": "Ophouden",
+          "ordered": "Besteld",
+          "sold": "Verkocht",
+          "sale": "Verkoop",
+          "wish": "Wens",
+          "region": "Region",
+          "ruler": "Ruler",
+          "denomination": "Benaming",
+          "subject": "Onderwerp",
+          "date_issue": "datum van uitgifte",
+          "year": "Jaar",
+          "mintage": "Gemunt",
+          "material": "Materiaal",
+          "mint": "Munthuis"
+        }
+    },
+    pl: {
+      translation: {
+          "status": "Status",
+          "country": "Kraj",
+          "series": "Seria",
+          "type": "Typ",
+          "period": "Okres",
+          "All": "All",
+          "owned": "Posiadane",
+          "demo": "Demo",
+          "pass": "Przekazano",
+          "ordered": "Zamówione",
+          "sold": "Sprzedane",
+          "sale": "Sprzedaż",
+          "wish": "Chcę",
+          "region": "Region",
+          "ruler": "Ruler",
+          "denomination": "Denominacja",
+          "subject": "Temat",
+          "date_issue": "Data emisji",
+          "year": "Rok",
+          "mintage": "Nakład",
+          "material": "Stop metali",
+          "mint": "Mennica"
+        }
+    },
+    pt: {
+      translation: {
+          "status": "Estado",
+          "country": "País",
+          "series": "Séries",
+          "type": "Tipo",
+          "period": "Período",
+          "All": "Tudo",
+          "owned": "Possuída",
+          "demo": "Demonstração",
+          "pass": "Troca",
+          "ordered": "Encomendada",
+          "sold": "Vendida",
+          "sale": "Vendável",
+          "wish": "Desejada",
+          "region": "Região",
+          "ruler": "Régua",
+          "denomination": "Denominação",
+          "subject": "Assunto",
+          "date_issue": "Data de emissão",
+          "year": "Ano",
+          "mintage": "Cunhagem",
+          "material": "Material",
+          "mint": "Cunho"
+        }
+    },
+    ru: {
+      translation: {
+          "status": "Статус",
+          "country": "Страна",
+          "series": "Серии",
+          "type": "Тип",
+          "period": "Период",
+          "All": "Все",
+          "owned": "Есть",
+          "demo": "Демо",
+          "pass": "Проход",
+          "ordered": "Заказана",
+          "sold": "Продана",
+          "sale": "На продажу",
+          "wish": "Нужна",
+          "region": "Регион",
+          "ruler": "Правитель",
+          "denomination": "Номинал",
+          "subject": "Тема",
+          "date_issue": "Дата выпуска",
+          "year": "Год",
+          "mintage": "Тираж",
+          "material": "Материал",
+          "mint": "Двор"
+        }
+    },
+    uk: {
+      translation: {
+          "status": "Статус",
+          "country": "Країна",
+          "series": "Серія",
+          "type": "Тип",
+          "period": "Перiод",
+          "All": "Усе",
+          "owned": "Є",
+          "demo": "Демо",
+          "pass": "Прохід",
+          "ordered": "Замовлена",
+          "sold": "Продана",
+          "sale": "Продаж",
+          "wish": "Потрібна",
+          "region": "Region",
+          "ruler": "Ruler",
+          "denomination": "Номінал",
+          "subject": "Тема",
+          "date_issue": "Дата випуску",
+          "year": "Рік",
+          "mintage": "Тираж",
+          "material": "Матеріал",
+          "mint": "Літери"
+        }
+    },
+  }
+});
+
 function filterChanged() {
     filters = [];
 
@@ -91,14 +488,14 @@ function applyFilter(commands) {
 function execute(commands) {
 	worker.onmessage = function(event) {
 		var results = event.data.results;
-
+        
         $('div#filters').empty();
         html = "<table>";
-        html += filterCreate('status', 'Status', results[1].values);
-        html += filterCreate('country', 'Country', results[2].values);
-        html += filterCreate('series', 'Series', results[3].values);
-        html += filterCreate('type', 'Type', results[4].values);
-        html += filterCreate('period', 'Period', results[5].values);
+        html += filterCreate('status', results[1].values);
+        html += filterCreate('country', results[2].values);
+        html += filterCreate('series', results[3].values);
+        html += filterCreate('type', results[4].values);
+        html += filterCreate('period', results[5].values);
         html += "</table>";
         $('div#filters').append(html);
 
@@ -116,9 +513,10 @@ function execute(commands) {
 
 // Create an HTML table
 var filterCreate = function () {
-  return function (id, label, values){
+  return function (id, values){
+    var label = i18next.t(id);
     var rows = values.map(function(v){ return '<option>' + v[0] + '</option>'});
-    var html = '<tr><td><label for="' + id + '">' + label + ':</label></td><td><select class="filter" id="' + id + '"><option>All</option>' + rows.join('') + '</select></td></tr>';
+    var html = '<tr><td><label for="' + id + '">' + label + ':</label></td><td><select class="filter" id="' + id + '"><option>' + i18next.t('All') + '</option>' + rows.join('') + '</select></td></tr>';
     return html;
   }
 }();
@@ -140,7 +538,7 @@ var tableCreate = function () {
         if (v[9])
             desc.push(v[9]);
         return '<tr class="row" data-id="' + v[0] + '"><td class="image"><img src="data:image/png;base64,' + arrayBufferToBase64(v[1]) + '"></td>\
-            <td class="data"><div class="title">' + v[2] + '&nbsp;</div><div class="description">' + desc.join(', ') + '&nbsp;</div></td><td class="status">' + v[3] + '</td></tr>';
+            <td class="data"><div class="title">' + v[2] + '&nbsp;</div><div class="description">' + desc.join(', ') + '&nbsp;</div></td><td class="status">' + i18next.t(v[3]) + '</td></tr>';
     });
     var html = '<table class="table">' + rows.join('') + '</table>';
     tbl.innerHTML = html;
@@ -183,35 +581,35 @@ var infoCreate = function () {
         images += '<div class="coin-image"><img src="data:image/png;base64,' + arrayBufferToBase64(v[2]) + '"></div>';
     var fields = '<table class="info">';
     if (v[3])
-        fields += '<tr><td class="min">Status:</td><td><b>' + v[3] + '</b></td></tr>';
+        fields += '<tr><td class="min">' + i18next.t('status') + ':</td><td><b>' + v[3] + '</b></td></tr>';
     if (v[4])
-        fields += '<tr><td class="min">Region:</td><td><b>' + v[4] + '</b></td></tr>';
+        fields += '<tr><td class="min">' + i18next.t('region') + ':</td><td><b>' + v[4] + '</b></td></tr>';
     if (v[5])
-        fields += '<tr><td class="min">Country:</td><td><b>' + v[5] + '</b></td></tr>';
+        fields += '<tr><td class="min">' + i18next.t('country') + ':</td><td><b>' + v[5] + '</b></td></tr>';
     if (v[6])
-        fields += '<tr><td class="min">Period:</td><td><b>' + v[6] + '</b></td></tr>';
+        fields += '<tr><td class="min">' + i18next.t('period') + ':</td><td><b>' + v[6] + '</b></td></tr>';
     if (v[7])
-        fields += '<tr><td class="min">Ruler:</td><td><b>' + v[7] + '</b></td></tr>';
+        fields += '<tr><td class="min">' + i18next.t('ruler') + ':</td><td><b>' + v[7] + '</b></td></tr>';
     if (v[8] || v[9])
-        fields += '<tr><td class="min">Denomination:</td><td><b>' + v[8] + ' ' + v[9] + '</b></td></tr>';
+        fields += '<tr><td class="min">' + i18next.t('denomination') + ':</td><td><b>' + v[8] + ' ' + v[9] + '</b></td></tr>';
     if (v[10])
-        fields += '<tr><td class="min">Type:</td><td><b>' + v[10] + '</b></td></tr>';
+        fields += '<tr><td class="min">' + i18next.t('type') + ':</td><td><b>' + v[10] + '</b></td></tr>';
     if (v[11])
-        fields += '<tr><td class="min">Series:</td><td><b>' + v[11] + '</b></td></tr>';
+        fields += '<tr><td class="min">' + i18next.t('series') + ':</td><td><b>' + v[11] + '</b></td></tr>';
     if (v[12])
-        fields += '<tr><td class="min">Subject:</td><td><b>' + v[12] + '</b></td></tr>';
+        fields += '<tr><td class="min">' + i18next.t('subject') + ':</td><td><b>' + v[12] + '</b></td></tr>';
     if (v[13])
-        fields += '<tr><td class="min">Date of issue:</td><td><b>' + v[13] + '</b></td></tr>';
+        fields += '<tr><td class="min">' + i18next.t('date_issue') + ':</td><td><b>' + v[13] + '</b></td></tr>';
     else if (v[14])
-        fields += '<tr><td class="min">Year:</td><td><b>' + v[14] + '</b></td></tr>';
+        fields += '<tr><td class="min">' + i18next.t('year') + ':</td><td><b>' + v[14] + '</b></td></tr>';
     if (v[15])
-        fields += '<tr><td class="min">Mintage:</td><td><b>' + v[15] + '</b></td></tr>';
+        fields += '<tr><td class="min">' + i18next.t('mintage') + ':</td><td><b>' + v[15] + '</b></td></tr>';
     if (v[16])
-        fields += '<tr><td class="min">Material:</td><td><b>' + v[16] + '</b></td></tr>';
+        fields += '<tr><td class="min">' + i18next.t('material') + ':</td><td><b>' + v[16] + '</b></td></tr>';
     if (v[17])
-        fields += '<tr><td class="min">Mint:</td><td><b>' + v[17] + '</b></td></tr>';
+        fields += '<tr><td class="min">' + i18next.t('mint') + ':</td><td><b>' + v[17] + '</b></td></tr>';
     else if (v[18])
-        fields += '<tr><td class="min">Mint:</td><td><b>' + v[18] + '</b></td></tr>';
+        fields += '<tr><td class="min">' + i18next.t('mint') + ':</td><td><b>' + v[18] + '</b></td></tr>';
     fields += '</table>';
     var html = title + images + fields;
     tbl.innerHTML = html;
