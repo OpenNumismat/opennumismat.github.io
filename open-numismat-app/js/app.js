7a57739c26ca32ac7cefcac70425f77622aa3ed3
Janis <opennumismat@gmail.com>
2021-09-27 14:39:25 +0300

    Check db version and password

--- open-numismat-app/js/app.js
+++ open-numismat-app/js/app.js
@@ -1,10 +1,7 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./service-worker.js')
-             .then(function(registration) {
-      console.log('Service Worker Registered');
-      registration.update();
-    });
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js');
+  });
 }
 
 var infoElm = document.getElementById('info');
@@ -17,17 +14,26 @@ var mainSqlSort = "";
 
 // Start the worker in which sql.js will run
 var worker = new Worker("js/worker.sql.js");
-worker.onerror = error;
+worker.onerror = errorSql;
 
 // Open a database
 worker.postMessage({action:'open'});
 
-function error(e) {
+function errorSql(e) {
     console.log(e);
+	error(e.message);
+}
+
+function error(msg) {
+    console.log(msg);
     $.mobile.loading( "hide" );
-    $("#error p").text(e.message);
+    $("#error p").text(msg);
     $("#error").popup("open");
 }
+
+$(document).bind('pageinit', function() {
+    $("#error").popup();
+});
 
 function status(text="") {
     if (text === "") {
@@ -100,6 +106,8 @@ i18next.init({
           "load_db": "Loading database from file",
           "BC": "BC",
           "density": "Density of the display: ",
+		  "old_version": "File has an old version. Please update it with desktop version of OpenNumismat",
+		  "wrong_password": "Wrong password",
           "content": {
             "open": "Open",
             "about": "About",
@@ -141,6 +149,8 @@ i18next.init({
           "load_db": "Зарежда се база данни от файл",
           "BC": "пр. Хр.",
           "density": "Показва плътността на пикселите:",
+		  "old_version": "File has an old version. Please update it with desktop version of OpenNumismat",
+		  "wrong_password": "Wrong password",
           "content": {
             "open": "Отваряне",
             "about": "Относно",
@@ -182,6 +192,8 @@ i18next.init({
           "load_db": "Loading database from file",
           "BC": "BC",
           "density": "Density of the display: ",
+		  "old_version": "File has an old version. Please update it with desktop version of OpenNumismat",
+		  "wrong_password": "Wrong password",
           "content": {
             "open": "Obre",
             "about": "Quant a",
@@ -223,6 +235,8 @@ i18next.init({
           "load_db": "Loading database from file",
           "BC": "BC",
           "density": "Density of the display: ",
+		  "old_version": "File has an old version. Please update it with desktop version of OpenNumismat",
+		  "wrong_password": "Wrong password",
           "content": {
             "open": "Otevřít",
             "about": "About",
@@ -264,6 +278,8 @@ i18next.init({
           "load_db": "Loading database from file",
           "BC": "v. Chr.",
           "density": "Density of the display: ",
+		  "old_version": "File has an old version. Please update it with desktop version of OpenNumismat",
+		  "wrong_password": "Wrong password",
           "content": {
             "open": "Öffnen",
             "about": "Über",
@@ -305,6 +321,8 @@ i18next.init({
           "load_db": "Loading database from file",
           "BC": "π.Χ.",
           "density": "Density of the display: ",
+		  "old_version": "File has an old version. Please update it with desktop version of OpenNumismat",
+		  "wrong_password": "Wrong password",
           "content": {
             "open": "Άνοιγμα",
             "about": "Περί",
@@ -346,6 +364,8 @@ i18next.init({
           "load_db": "Loading database from file",
           "BC": "AC",
           "density": "Density of the display: ",
+		  "old_version": "File has an old version. Please update it with desktop version of OpenNumismat",
+		  "wrong_password": "Wrong password",
           "content": {
             "open": "Abrir",
             "about": "Acerca de",
@@ -387,6 +407,8 @@ i18next.init({
           "load_db": "Loading database from file",
           "BC": "BC",
           "density": "Density of the display: ",
+		  "old_version": "File has an old version. Please update it with desktop version of OpenNumismat",
+		  "wrong_password": "Wrong password",
           "content": {
             "open": "Ouvrir",
             "about": "À propos",
@@ -428,6 +450,8 @@ i18next.init({
           "load_db": "Loading database from file",
           "BC": "BC",
           "density": "Density of the display: ",
+		  "old_version": "File has an old version. Please update it with desktop version of OpenNumismat",
+		  "wrong_password": "Wrong password",
           "content": {
             "open": "Megnyitás",
             "about": "About",
@@ -469,6 +493,8 @@ i18next.init({
           "load_db": "Loading database from file",
           "BC": "BC",
           "density": "Density of the display: ",
+		  "old_version": "File has an old version. Please update it with desktop version of OpenNumismat",
+		  "wrong_password": "Wrong password",
           "content": {
             "open": "Apri",
             "about": "A riguardo",
@@ -510,6 +536,8 @@ i18next.init({
           "load_db": "Loading database from file",
           "BC": "BC",
           "density": "Density of the display: ",
+		  "old_version": "File has an old version. Please update it with desktop version of OpenNumismat",
+		  "wrong_password": "Wrong password",
           "content": {
             "open": "Open",
             "about": "Over",
@@ -551,6 +579,8 @@ i18next.init({
           "load_db": "Loading database from file",
           "BC": "BC",
           "density": "Density of the display: ",
+		  "old_version": "File has an old version. Please update it with desktop version of OpenNumismat",
+		  "wrong_password": "Wrong password",
           "content": {
             "open": "Otwórz",
             "about": "O nas",
@@ -592,6 +622,8 @@ i18next.init({
           "load_db": "A carregar base de dados de ficheiro",
           "BC": "BC",
           "density": "Densidade do monitor:",
+		  "old_version": "File has an old version. Please update it with desktop version of OpenNumismat",
+		  "wrong_password": "Wrong password",
           "content": {
             "open": "Abrir",
             "about": "Sobre",
@@ -633,6 +665,8 @@ i18next.init({
           "load_db": "Загрузка базы данных из файла",
           "BC": "До РХ",
           "density": "Плотность пикселей дисплея: ",
+		  "old_version": "File has an old version. Please update it with desktop version of OpenNumismat",
+		  "wrong_password": "Wrong password",
           "content": {
             "open": "Открыть",
             "about": "О программе",
@@ -674,6 +708,8 @@ i18next.init({
           "load_db": "Loading database from file",
           "BC": "BC",
           "density": "Density of the display: ",
+		  "old_version": "File has an old version. Please update it with desktop version of OpenNumismat",
+		  "wrong_password": "Wrong password",
           "content": {
             "open": "Відкрити",
             "about": "Про программу",
@@ -767,7 +803,69 @@ function applyFilter(commands) {
     status(i18next.t('build_table'));
 }
 
-function execute(commands) {
+function checkVersion() {
+	sql = "SELECT value FROM settings WHERE title='Version';";
+
+	worker.onmessage = function(event) {
+		var results = event.data.results;
+		var version = Number(results[0].values[0][0]);
+
+		status();
+			
+		if (version < 7)
+			error(i18next.t('old_version'));
+		else
+			checkPassword();
+	}
+
+    status(i18next.t('exec_sql'));
+	worker.postMessage({action:'exec', sql:sql});
+}
+
+function MD5(d){var r = M(V(Y(X(d),8*d.length)));return r.toLowerCase()};function M(d){for(var _,m="0123456789ABCDEF",f="",r=0;r<d.length;r++)_=d.charCodeAt(r),f+=m.charAt(_>>>4&15)+m.charAt(15&_);return f}function X(d){for(var _=Array(d.length>>2),m=0;m<_.length;m++)_[m]=0;for(m=0;m<8*d.length;m+=8)_[m>>5]|=(255&d.charCodeAt(m/8))<<m%32;return _}function V(d){for(var _="",m=0;m<32*d.length;m+=8)_+=String.fromCharCode(d[m>>5]>>>m%32&255);return _}function Y(d,_){d[_>>5]|=128<<_%32,d[14+(_+64>>>9<<4)]=_;for(var m=1732584193,f=-271733879,r=-1732584194,i=271733878,n=0;n<d.length;n+=16){var h=m,t=f,g=r,e=i;f=md5_ii(f=md5_ii(f=md5_ii(f=md5_ii(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_ff(f=md5_ff(f=md5_ff(f=md5_ff(f,r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+0],7,-680876936),f,r,d[n+1],12,-389564586),m,f,d[n+2],17,606105819),i,m,d[n+3],22,-1044525330),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+4],7,-176418897),f,r,d[n+5],12,1200080426),m,f,d[n+6],17,-1473231341),i,m,d[n+7],22,-45705983),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+8],7,1770035416),f,r,d[n+9],12,-1958414417),m,f,d[n+10],17,-42063),i,m,d[n+11],22,-1990404162),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+12],7,1804603682),f,r,d[n+13],12,-40341101),m,f,d[n+14],17,-1502002290),i,m,d[n+15],22,1236535329),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+1],5,-165796510),f,r,d[n+6],9,-1069501632),m,f,d[n+11],14,643717713),i,m,d[n+0],20,-373897302),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+5],5,-701558691),f,r,d[n+10],9,38016083),m,f,d[n+15],14,-660478335),i,m,d[n+4],20,-405537848),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+9],5,568446438),f,r,d[n+14],9,-1019803690),m,f,d[n+3],14,-187363961),i,m,d[n+8],20,1163531501),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+13],5,-1444681467),f,r,d[n+2],9,-51403784),m,f,d[n+7],14,1735328473),i,m,d[n+12],20,-1926607734),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+5],4,-378558),f,r,d[n+8],11,-2022574463),m,f,d[n+11],16,1839030562),i,m,d[n+14],23,-35309556),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+1],4,-1530992060),f,r,d[n+4],11,1272893353),m,f,d[n+7],16,-155497632),i,m,d[n+10],23,-1094730640),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+13],4,681279174),f,r,d[n+0],11,-358537222),m,f,d[n+3],16,-722521979),i,m,d[n+6],23,76029189),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+9],4,-640364487),f,r,d[n+12],11,-421815835),m,f,d[n+15],16,530742520),i,m,d[n+2],23,-995338651),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+0],6,-198630844),f,r,d[n+7],10,1126891415),m,f,d[n+14],15,-1416354905),i,m,d[n+5],21,-57434055),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+12],6,1700485571),f,r,d[n+3],10,-1894986606),m,f,d[n+10],15,-1051523),i,m,d[n+1],21,-2054922799),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+8],6,1873313359),f,r,d[n+15],10,-30611744),m,f,d[n+6],15,-1560198380),i,m,d[n+13],21,1309151649),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+4],6,-145523070),f,r,d[n+11],10,-1120210379),m,f,d[n+2],15,718787259),i,m,d[n+9],21,-343485551),m=safe_add(m,h),f=safe_add(f,t),r=safe_add(r,g),i=safe_add(i,e)}return Array(m,f,r,i)}function md5_cmn(d,_,m,f,r,i){return safe_add(bit_rol(safe_add(safe_add(_,d),safe_add(f,i)),r),m)}function md5_ff(d,_,m,f,r,i,n){return md5_cmn(_&m|~_&f,d,_,r,i,n)}function md5_gg(d,_,m,f,r,i,n){return md5_cmn(_&f|m&~f,d,_,r,i,n)}function md5_hh(d,_,m,f,r,i,n){return md5_cmn(_^m^f,d,_,r,i,n)}function md5_ii(d,_,m,f,r,i,n){return md5_cmn(m^(_|~f),d,_,r,i,n)}function safe_add(d,_){var m=(65535&d)+(65535&_);return(d>>16)+(_>>16)+(m>>16)<<16|65535&m}function bit_rol(d,_){return d<<_|d>>>32-_}
+
+function checkPassword() {
+	sql = "SELECT value FROM settings WHERE title='Password';";
+
+	worker.onmessage = function(event) {
+		var results = event.data.results;
+		var hashed_password = results[0].values[0][0];
+
+		status();
+
+		if (MD5('') == hashed_password) {
+			execute();
+		}
+		else {
+			$("#popupPassword").on({
+				popupafterclose: function(event, ui) {
+					var password = $("#pw").val();
+					$("#pw").val('');
+					if (MD5(password) == hashed_password) {
+						execute();
+					}
+					else {
+						error(i18next.t('wrong_password'));
+					}
+				}
+			});
+			$("#popupPassword").popup().popup("open");
+		}
+	}
+
+    status(i18next.t('exec_sql'));
+	worker.postMessage({action:'exec', sql:sql});
+}
+
+function execute() {
+	sql = mainSqlSelect + ";\
+				SELECT DISTINCT status FROM coins;\
+				SELECT DISTINCT country FROM coins;\
+				SELECT DISTINCT series FROM coins;\
+				SELECT DISTINCT type FROM coins;\
+				SELECT DISTINCT period FROM coins;\
+				SELECT DISTINCT mint FROM coins;";
+
 	worker.onmessage = function(event) {
 		var results = event.data.results;
 
@@ -813,7 +911,7 @@ function execute(commands) {
         status();
 	}
     status(i18next.t('exec_sql'));
-	worker.postMessage({action:'exec', sql:commands});
+	worker.postMessage({action:'exec', sql:sql});
     status(i18next.t('build_table'));
 }
 
@@ -1031,13 +1129,7 @@ dbFileElm.onchange = function() {
         mainSqlSort = "";
         r.onload = function() {
             worker.onmessage = function () {
-                execute (mainSqlSelect + ";\
-                    SELECT DISTINCT status FROM coins;\
-                    SELECT DISTINCT country FROM coins;\
-                    SELECT DISTINCT series FROM coins;\
-                    SELECT DISTINCT type FROM coins;\
-                    SELECT DISTINCT period FROM coins;\
-                    SELECT DISTINCT mint FROM coins;");
+				checkVersion();
             };
             try {
                 worker.postMessage({action:'open',buffer:r.result}, [r.result]);
