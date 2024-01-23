0cc51be66ba11fa080bbfac4180fc9591dee5fc7
Janis <opennumismat@gmail.com>
2024-01-23 10:21:30 +0300

    Upgrade jQuery

--- javascripts/load_catalogues.js
+++ javascripts/load_catalogues.js
@@ -3,17 +3,17 @@ var url = "/data/catalogues_list.json";
 $(function() {
   $.getJSON( url, function( data ) {
     var i;
-    var arr = data["catalogues"]
-    for (i = 0; i < arr.length; i++) {
-      file = arr[i]['file'];
+    var catalogues = data["catalogues"]
+    for (catalog of catalogues) {
+      file = catalog['file'];
       name = file.substr(0, file.length-3);
-      url = 'https://github.com/OpenNumismat/catalogues/releases/download/' + name + '_' + arr[i]['date'] + '/' + name + '.db'
+      url = 'https://github.com/OpenNumismat/catalogues/releases/download/' + name + '_' + catalog['date'] + '/' + name + '.db'
       row = '<li>';
-      row += arr[i]['title'] + ': ';
-      row += '<a href="' + url + '" rel="nofollow">download</a>, ';
-      row += '<a href="catalogue.html?catalogue=' + name + '&date=' + arr[i]['date'] + '" rel="nofollow">preview</a> ';
-      row += '(at ' + arr[i]['date'] + ', ';
-      row += arr[i]['count'] + ' coins);';
+      row += catalog['title'] + ': ';
+      row += '<a href="' + url + '">download</a>, ';
+      row += '<a href="catalogue.html?catalogue=' + name + '&date=' + catalog['date'] + '">preview</a> ';
+      row += '(at ' + catalog['date'] + ', ';
+      row += catalog['count'] + ' coins);';
       row += '</li>';
       $('#catalogues-list').append(row);
     }
